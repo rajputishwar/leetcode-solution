6a784f69029af8a9326f560e09704983abf7805d
rajputishwar <160762218+rajputishwar@users.noreply.github.com>
2025-04-19 23:42:52 +0530

    Time: 12 ms (81.15%), Space: 45.7 MB (52.29%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,37 @@
+class Element implements Comparable<Element>{
+    char ch;
+    int freq;
+    public Element(char ch,int freq){
+        this.ch=ch;
+        this.freq=freq;
+    }
+
+    public int compareTo(Element that){
+        return that.freq-this.freq;//dec
+    }
+}
+
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character,Integer> map = new HashMap<> ();
+        for(char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        PriorityQueue<Element> pq = new PriorityQueue<>();
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            Element element = new Element(entry.getKey(),entry.getValue());
+            pq.offer(element);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(pq.size()>0){
+            Element element = pq.poll();
+            while(element.freq>0){
+                sb.append(element.ch);
+                element.freq--;
+            }
+        }
+
+        return sb.toString();
+    }
+}
