0610a5bcfcf4fd882ef5d2bc8f7437fccb163fa1
rajputishwar <160762218+rajputishwar@users.noreply.github.com>
2025-04-19 23:16:05 +0530

    Time: 6 ms (43.53%), Space: 43.1 MB (84.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,11 +5,11 @@ class Solution {
     return false;
  }
 
- int freq[] = new int[256];
+ int freq[] = new int[26];
 
  for(int i = 0;i<s.length();i++){
-    freq[s.charAt(i)-0]++;
-    freq[t.charAt(i)-0]--;
+    freq[s.charAt(i)-'a']++;
+    freq[t.charAt(i)-'a']--;
  }
 
  for(int i = 0;i<freq.length;i++){
